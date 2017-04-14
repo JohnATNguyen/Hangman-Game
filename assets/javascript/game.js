47c5d68a9c45a8b96b68b88c26047600c8baf86d
John Nguyen <John Nguyen>
2017-04-13 19:54:26 -0700

    4/13/17 @ 7:54pm (finished and audited)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,9 +12,69 @@ var losses = 0;
 
 var californialove = new Audio('assets/2Pac_CaliforniaLoveOriginalVersion_OldSchool_320VBR.mp3');
 
+function play() {
+
+	// respond to user input
+	document.onkeyup = function (event) {
+		if (event.key === '`' || event.key == '~' || event.key == '1' || event.key == '!' || event.key == '2' || event.key == '@' || event.key == '3' || event.key == '#' || event.key == '4' || event.key == '$' || event.key == '5' || event.key == '%' || event.key == '6' || event.key == '^' || event.key == '7' || event.key == '&' || event.key == '8' || event.key == '*' || event.key == '9' || event.key == '(' || event.key == '0' || event.key == ')' || event.key == '-' || event.key == '_' || event.key == '=' || event.key == '+' || event.key == 'q' || event.key == 'w' || event.key == 'e' || event.key == 'r' || event.key == 't' || event.key == 'y' || event.key == 'u' || event.key == 'i' || event.key == 'o' || event.key == 'p' || event.key == '[' || event.key == '{' || event.key == ']' || event.key == '}' || event.key == "\\" || event.key == '|' || event.key == 'a' || event.key == 's' || event.key == 'd' || event.key == 'f' || event.key == 'g' || event.key == 'h' || event.key == 'j' || event.key == 'k' || event.key == 'l' || event.key == ';' || event.key == ':' || event.key == '\'' || event.key == '"' || event.key == 'z' || event.key == 'x' || event.key == 'c' || event.key == 'v' || event.key == 'b' || event.key == 'n' || event.key == 'm' || event.key == ',' || event.key == '<' || event.key == '.' || event.key == '>' || event.key == '/' || event.key == '?' || event.key == ' ') {
+			var isInWord = false;
+			for (var i = 0; i < wordToGuessLower.length; i++) {
+				// if user input matches a character in song name
+				if (event.key === wordToGuessLower[i]) {
+					isInWord = true;
+					placeholder[i] = event.key;
+					document.getElementById('wordtoguesselement').textContent = placeholder;
+				}
+			}
+
+			// if user input does not match a character in song name
+			if (isInWord === false) {
+				// take action only if it's a fresh wrong letter
+				var isNewInWrongGuesses = true;
+				for (var i = 0; i < wrongGuesses.length; i++) {
+					if (event.key === wrongGuesses[i]) {
+						isNewInWrongGuesses = false;
+					}
+				}
+				if (isNewInWrongGuesses === true) {
+					wrongGuesses.push(event.key);
+					document.getElementById('wrongguesseselement').textContent = 'Wrong Guesses: ' + wrongGuesses;
+					if (guessesLeft > 0) {
+						guessesLeft--;
+						document.getElementById('guessesleftelement').textContent = 'Guesses Left: ' + guessesLeft;
+					}
+				}
+			}
+
+			// check if won
+			var stringplaceholder = placeholder.join('');
+			console.log(stringplaceholder);
+			console.log(wordToGuessLower);
+			if (stringplaceholder == wordToGuessLower) {
+				wins++;
+				document.getElementById('winselement').textContent = 'Wins: ' + wins;
+				stringplaceholder = wordToGuess;
+				document.getElementById('wordtoguesselement').textContent = stringplaceholder;
+				document.getElementById('picture').innerHTML = '<img src="assets/images/hqdefault.jpg" alt="westside">';
+				californialove.play();
+				document.getElementById("start").disabled = false;
+				document.onkeyup = null;
+			}
+
+			// check if lost
+			else if (guessesLeft === 0) {
+				losses++;
+				document.getElementById('losseselement').textContent = 'Losses: ' + losses;
+				alert('You let \'Pac down... (a.k.a. YOU LOST)!!! Press OK to retry =)')
+				initialize();
+			}
+		}
+	}
+}
+
 // define function to initialize game
 function initialize() {
-	document.getElementById('picture').innerHTML = '<img src="assets/images/tupac-shakur.jpg">';
+	document.getElementById('picture').innerHTML = '<img src="assets/images/tupac-shakur.jpg" alt="blackandwhite">';
 	wordToGuess = songs[Math.floor(Math.random() * songs.length)];
 	wordToGuessLower = wordToGuess.toLowerCase();
 	placeholder = [];
@@ -31,61 +91,9 @@ function initialize() {
 	document.getElementById("start").disabled = true;
 	californialove.pause();
 	californialove.currentTime = 0;
-}
-
-// respond to user input
-document.onkeyup = function (event) {
-	if (event.key === '`' || event.key == '~' || event.key == '1' || event.key == '!' || event.key == '2' || event.key == '@' || event.key == '3' || event.key == '#' || event.key == '4' || event.key == '$' || event.key == '5' || event.key == '%' || event.key == '6' || event.key == '^' || event.key == '7' || event.key == '&' || event.key == '8' || event.key == '*' || event.key == '9' || event.key == '(' || event.key == '0' || event.key == ')' || event.key == '-' || event.key == '_' || event.key == '=' || event.key == '+' || event.key == 'q' || event.key == 'w' || event.key == 'e' || event.key == 'r' || event.key == 't' || event.key == 'y' || event.key == 'u' || event.key == 'i' || event.key == 'o' || event.key == 'p' || event.key == '[' || event.key == '{' || event.key == ']' || event.key == '}' || event.key == "\\" || event.key == '|' || event.key == 'a' || event.key == 's' || event.key == 'd' || event.key == 'f' || event.key == 'g' || event.key == 'h' || event.key == 'j' || event.key == 'k' || event.key == 'l' || event.key == ';' || event.key == ':' || event.key == '\'' || event.key == '"' || event.key == 'z' || event.key == 'x' || event.key == 'c' || event.key == 'v' || event.key == 'b' || event.key == 'n' || event.key == 'm' || event.key == ',' || event.key == '<' || event.key == '.' || event.key == '>' || event.key == '/' || event.key == '?' || event.key == ' ') {
-		var isInWord = false;
-		for (var i = 0; i < wordToGuessLower.length; i++) {
-			// if user input matches a character in song name
-			if (event.key === wordToGuessLower[i]) {
-				isInWord = true;
-				placeholder[i] = event.key;
-				document.getElementById('wordtoguesselement').textContent = placeholder;
-			}
-		}
-
-		// if user input does not match a character in song name
-		if (isInWord === false) {
-			// take action only if it's a fresh wrong letter
-			var isNewInWrongGuesses = true;
-			for (var i = 0; i < wrongGuesses.length; i++) {
-				if (event.key === wrongGuesses[i]) {
-					isNewInWrongGuesses = false;
-				}
-			}
-			if (isNewInWrongGuesses === true) {
-				wrongGuesses.push(event.key);
-				document.getElementById('wrongguesseselement').textContent = 'Wrong Guesses: ' + wrongGuesses;
-				if (guessesLeft > 0) {
-					guessesLeft--;
-					document.getElementById('guessesleftelement').textContent = 'Guesses Left: ' + guessesLeft;
-				}
-			}
-		}
-
-		// check if won
-		var stringplaceholder = placeholder.join('');
-		console.log(stringplaceholder);
-		console.log(wordToGuessLower);
-		if (stringplaceholder == wordToGuessLower) {
-			wins++;
-			document.getElementById('winselement').textContent = 'Wins: ' + wins;
-			stringplaceholder = wordToGuess;
-			document.getElementById('wordtoguesselement').textContent = stringplaceholder;
-			document.getElementById('picture').innerHTML = '<img src="assets/images/hqdefault.jpg" alt="westside">';
-			californialove.play();
-			document.getElementById("start").disabled = false;
-		}
-
-		// check if lost
-		else if (guessesLeft === 0) {
-			losses++;
-			document.getElementById('losseselement').textContent = 'Losses: ' + losses;
-			alert('You let \'Pac down... (YOU LOST)!!!')
-			initialize();
-			document.getElementById("start").disabled = false;
-		}
+	if (document.onkeyup == null) {
+		play();
 	}
 }
+
+play();
